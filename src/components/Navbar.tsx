9a81257b378587e7c700dd7efc6e2f72449717b7
heathernfran <heathernfran@gmail.com>
2023-05-20 13:49:07 -0700

    Display nav links inline, remove unused styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,20 @@
 import * as React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
 
-const pageStyles = {
-  color: '#232129',
-  padding: 96,
-  fontFamily: '-apple-system, Roboto, sans-serif, serif'
-}
-const paragraphStyles = {
-  marginBottom: 48
-}
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4
-}
 const listStyles = {
-  marginBottom: 96,
+  marginBottom: 48,
   paddingLeft: 0
 }
-const doclistStyles = {
-  paddingLeft: 0
-}
+
 const listItemStyles = {
   fontWeight: 300,
   fontSize: 24,
   maxWidth: 560,
-  marginBottom: 30
+  marginBottom: 30,
+  listStyleType: 'none',
+  display: 'inline-block',
+  marginRight: 12,
+  paddingLeft: 0
 }
 
 const linkStyle = {
@@ -35,22 +22,6 @@ const linkStyle = {
   fontWeight: 'bold',
   fontSize: 16,
   verticalAlign: '5%'
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: 'none',
-  display: 'inline-block',
-  marginBottom: 24,
-  marginRight: 12
-}
-
-const descriptionStyle = {
-  color: '#232129',
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25
 }
 
 export interface Page {
