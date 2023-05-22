1972dadb304d514a03668d538d715d5d5644e335
heathernfran <heathernfran@gmail.com>
2023-05-22 11:31:46 -0700

    Add eslint plugin for jsx-a11y

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
     "plugin:@typescript-eslint/recommended",
     "eslint-config-prettier",
   ],
